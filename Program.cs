72f17bf900681460655e46d009274ac150e9aaee
Мартин Петров <114092342+mrtn03@users.noreply.github.com>
2022-09-23 20:28:59 +0300

    first steps in SoftUni

--- Program.cs
+++ Program.cs
@@ -1,20 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Inches_to_Centimeters
+
+namespace Hello_SoftUni
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Reading the input from the user
-            double number = double.Parse(Console.ReadLine());
-            // Logical convertion
-            double inches = 2.54;
-            double centimeters = number * inches;
-            Console.WriteLine(centimeters);
-
+            Console.WriteLine("Hello SoftUni");
         }
     }
 }
