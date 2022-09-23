3f3810183f3cb504ec4948ef6623bf3ea219f361
Мартин Петров <114092342+mrtn03@users.noreply.github.com>
2022-09-23 20:28:36 +0300

    Допълнителни задачи от училище и Programmin Basics

--- Program.cs
+++ Program.cs
@@ -2,22 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Nums_1to10
+namespace Inches_to_Centimeters
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(1);
-            Console.WriteLine(2);
-            Console.WriteLine(3);
-            Console.WriteLine(4);
-            Console.WriteLine(5);
-            Console.WriteLine(6);
-            Console.WriteLine(7);
-            Console.WriteLine(8);
-            Console.WriteLine(9);
-            Console.WriteLine(10);
+            // Reading the input from the user
+            double number = double.Parse(Console.ReadLine());
+            // Logical convertion
+            double inches = 2.54;
+            double centimeters = number * inches;
+            Console.WriteLine(centimeters);
+
         }
     }
 }
