99e681b1d9b919e8594a66d33eb486651c040ddc
Мартин Петров <114092342+mrtn03@users.noreply.github.com>
2022-09-23 20:28:18 +0300

    Допълнителни задачи от училище и Programmin Basics

--- Program.cs
+++ Program.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Projects_Creation
+namespace Nums_1to10
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string nameOfTheArchitect = Console.ReadLine();
-            int numberOfProcjects = int.Parse(Console.ReadLine());
-            int individualProjectTime = 3;
-            int timeForALLprojects = numberOfProcjects * individualProjectTime;
-            Console.WriteLine($"The architect {nameOfTheArchitect} will need {timeForALLprojects} hours to complete {numberOfProcjects} project/s.");
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            Console.WriteLine(6);
+            Console.WriteLine(7);
+            Console.WriteLine(8);
+            Console.WriteLine(9);
+            Console.WriteLine(10);
         }
     }
 }
