a7e39be2cb11b4e41982c4ccf6806524d5e478e9
Мартин Петров <114092342+mrtn03@users.noreply.github.com>
2022-09-23 20:38:18 +0300

    Programmin Basics 2022

--- Program.cs
+++ Program.cs
@@ -1,38 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace _05.SuppliesforSchool
+namespace _04.VacationBooksList
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //•	Пакет химикали - 5.80 лв. 
-            // •	Пакет маркери -7.20 лв.
-            // •	Препарат - 1.20 лв(за литър)
+            // 1.	Брой страници в текущата книга – цяло число в интервала [1…1000]
+            // 2.Страници, които прочита за 1 час – цяло число в интервала[1…1000]
+            // 3.Броят на дните, за които трябва да прочете книгата – цяло число в интервала[1…1000]
 
-            double paketHimikali = 5.8;
-            double paketMarkeri = 7.2;
-            double preparat = 1.2;
-            //•	Процент намаление -цяло число в интервала[0...100]
-
-            //Input
-            int cenaZaPaketiteHimikali = int.Parse(Console.ReadLine());
-            int cenaZaPaketMarkeri = int.Parse(Console.ReadLine());
-            int cenaZaPreparat = int.Parse(Console.ReadLine());
-            int percent = int.Parse(Console.ReadLine());
-            //Calculations
-            double krainaCenaZaHimikali = paketHimikali * cenaZaPaketiteHimikali;
-            double krainaCenaZaMarkeri = paketMarkeri * cenaZaPaketMarkeri;
-            double krainaCenaZaPreparat = preparat * cenaZaPreparat;
-            double sumForAllMaterials = krainaCenaZaHimikali + krainaCenaZaMarkeri + krainaCenaZaPreparat;
-            double sumWithDiscount = sumForAllMaterials - (sumForAllMaterials * percent / 100);
-
+            // Input
+            int broiStraniciKniga = int.Parse(Console.ReadLine());
+            int straniciZaChas = int.Parse(Console.ReadLine());
+            int broqNaDniteZaKoitoTrqbvaDaProcheteKnigata = int.Parse(Console.ReadLine());
+            // Calculations
+            int timeForRead = broiStraniciKniga / straniciZaChas;
+            int neobhodimiChasoveNaDen = timeForRead / broqNaDniteZaKoitoTrqbvaDaProcheteKnigata;
             // Output
-            Console.WriteLine(sumWithDiscount);
-
-
+            Console.WriteLine(neobhodimiChasoveNaDen);
         }
     }
 }
