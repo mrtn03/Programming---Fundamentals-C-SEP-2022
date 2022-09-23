ff668aab2249c3c6a054d1e3e788bfc9719d7114
Мартин Петров <114092342+mrtn03@users.noreply.github.com>
2022-09-23 20:20:13 +0300

    Programmin Basics 2022

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USD_към_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+            Console.WriteLine(bgn);
+        }
+    }
+}
